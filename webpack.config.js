ce61ca30ef7ea5b322e47b32904ea546c7bf329d
Ulysses <ulyssesgoncalves@digix.com.br>
2022-12-01 17:05:59 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin')
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './app/src/js/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'app/dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerWebpackPlugin(),
+    '...']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './app/src/app.html',
+      filename: 'app.html',
+      hash: true,
+    }),
+    new MiniCssExtractPlugin({
+        filename: 'style.css'
+    }),
+    new webpack.optimize.ModuleConcatenationPlugin()
+  ]
+};
